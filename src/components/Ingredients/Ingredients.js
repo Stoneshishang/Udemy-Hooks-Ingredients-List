2ef7b921c435b27a48377092c8596cc1bf2fec9f
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-09-02 09:40:20 -0400

    useReducer replaced useState if the State updates are inter connected

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useState, useEffect, useCallback } from "react";
+import React, { useReducer, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -18,11 +18,30 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 };
 
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case "SEND":
+      return { loading: true, error: null };
+    case "RESPONSE":
+      return { ...curHttpState, loading: false };
+    case "ERROR":
+      return { loading: false, error: action.errorMessage };
+    case "CLEAR":
+      return { ...curHttpState, error: null };
+    default:
+      throw new Error("Should not be reached!");
+  }
+};
+
 const Ingredients = () => {
   const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  const [httpState, dispatchHTTP] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
   // const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
 
   useEffect(() => {
     console.log("RENDERING INGREDIENTS", userIngredients);
@@ -36,14 +55,16 @@ const Ingredients = () => {
   // https://console.firebase.google.com/u/0/project/react-hooks-update-faf1a/database/react-hooks-update-faf1a/data
   // try ' git config --global core.safecrlf false ' to get ride of the warning when git add .
   const addIngredientHandler = (ingredient) => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHTTP({ type: "SEND" }); //this action doesn't need any old data to work. see above httpReducer return statement
     fetch("https://react-hooks-update-faf1a.firebaseio.com/ingredients.json", {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" },
     })
       .then((response) => {
-        setIsLoading(false);
+        // setIsLoading(false);
+        dispatchHTTP({ type: "RESPONSE" });
         return response.json();
       })
       .then((responseData) => {
@@ -59,7 +80,8 @@ const Ingredients = () => {
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHTTP({ type: "SEND" });
     fetch(
       `https://react-hooks-update-faf1a.firebaseio.com/ingredients/${ingredientId}.json`,
       {
@@ -67,28 +89,36 @@ const Ingredients = () => {
       }
     )
       .then((response) => {
-        setIsLoading(false);
+        // setIsLoading(false);
+        dispatchHTTP({ type: "RESPONSE" });
         // setUserIngredients((prevIngredients) =>
         //   prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
         // );
         dispatch({ type: "DELETE", id: ingredientId });
       })
       .catch((error) => {
-        setError("Something went wrong, please contact developer.");
-        setIsLoading(false);
+        // setError("Something went wrong, please contact developer.");
+        // setIsLoading(false);
+        dispatchHTTP({
+          type: "ERROR",
+          errorMessage: "Something went wrong, please contact developer!",
+        });
       });
   };
 
   const clearError = () => {
-    setError(null);
+    // setError(null);
+    dispatchHTTP({ type: "CLEAR" });
   };
 
   return (
     <div className='App'>
-      {error && <ErrorModel onClose={clearError}>{error}</ErrorModel>}
+      {httpState.error && (
+        <ErrorModel onClose={clearError}>{httpState.error}</ErrorModel>
+      )}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
-        loading={isLoading}
+        loading={httpState.loading}
       />
 
       <section>
