bba87a61852839671cf27738e680530e7286fe70
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2020-09-02 10:07:00 -0400

    useState only, no useReducer yet

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,82 +1,55 @@
-import React, { useReducer, useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 
-const ingredientReducer = (currentIngredients, action) => {
-  switch (action.type) {
-    case "SET":
-      return action.ingredients;
-    case "ADD":
-      return [...currentIngredients, action.ingredient];
-    case "DELETE":
-      return currentIngredients.filter((ing) => ing.id !== action.id);
-    default:
-      throw new Error("Should not get here!");
-  }
-};
-
 const Ingredients = () => {
-  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
-  // const [userIngredients, setUserIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [userIngredients, setUserIngredients] = useState([]);
 
   useEffect(() => {
     console.log("RENDERING INGREDIENTS", userIngredients);
   }, [userIngredients]);
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
-    // setUserIngredients(filteredIngredients);
-    dispatch({ type: "SET", ingredients: filteredIngredients });
+    setUserIngredients(filteredIngredients);
   }, []);
 
   // https://console.firebase.google.com/u/0/project/react-hooks-update-faf1a/database/react-hooks-update-faf1a/data
   // try ' git config --global core.safecrlf false ' to get ride of the warning when git add .
   const addIngredientHandler = (ingredient) => {
-    setIsLoading(true);
     fetch("https://react-hooks-update-faf1a.firebaseio.com/ingredients.json", {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" },
     })
       .then((response) => {
-        setIsLoading(false);
         return response.json();
       })
       .then((responseData) => {
-        // setUserIngredients((prevIngredients) => [
-        //   ...prevIngredients,
-        //   { id: responseData.name, ...ingredient },
-        // ]);
-        dispatch({
-          type: "ADD",
-          ingredient: { id: responseData.name, ...ingredient },
-        });
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
       });
   };
 
   const removeIngredientHandler = (ingredientId) => {
-    setIsLoading(true);
     fetch(
       `https://react-hooks-update-faf1a.firebaseio.com/ingredients/${ingredientId}.json`,
       {
         method: "DELETE",
       }
     ).then((response) => {
-      setIsLoading(false);
-      // setUserIngredients((prevIngredients) =>
-      //   prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-      // );
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+      );
     });
   };
 
   return (
     <div className='App'>
-      <IngredientForm
-        onAddIngredient={addIngredientHandler}
-        loading={isLoading}
-      />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
